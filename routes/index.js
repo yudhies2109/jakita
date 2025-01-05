6c59bb953c1fb3d220eb920745d6909576f2ec9f
yudhies2109 <ma.yudhistira@hotmail.com>
2025-01-06 02:29:48 +0700

    login and logout added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Yudhistira' });
-});
+module.exports = function (pool) {
+  router.post('/', function(req, res) {
+    let email = req.body.email;
+    let pass = req.body.password;
+    let sql = `SELECT COUNT(email) as count FROM jakita_user WHERE email = '${email}' AND password ='${pass}'`;
+  
+    pool.query(sql, function(err, data) {
+      let JmlhData = data.rows[0].count;
+  
+      if (err) {
+        throw err;
+      }
+  
+      if (JmlhData > 0) {
+        req.session.email = email;
+        const response = {
+          status: "succes",
+          message: "Login Berhasil",
+          data : {
+            email: email
+          }
+        }
+        console.log(JSON.stringify(response, null, 2)); 
+      } else {
+        req.flash('loginMessage', 'Masukkan Username & Password Dengan Benar');
+        const response = {
+          status: "error",
+          message: "Login gagal!"
+        };
+        console.log(JSON.stringify(response, null, 2));
+        // Tetap di halaman login
+        // res.redirect('/');  // Untuk pengujian lebih lanjut, gunakan redirect ini
+      }
+    });
+  });
+  
+  router.get('/logout', function(req, res){
+    req.session.destroy(function(err) {
+      console.log('TERIMA KASIH!');
+      // res.redirect('/');
+    })
+  });
 
-module.exports = router;
+  return router;
+};
