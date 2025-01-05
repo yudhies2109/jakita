824018e10a5a428a2d06f2885c1cb595f5fd4852
yudhies2109 <ma.yudhistira@hotmail.com>
2025-01-05 22:31:15 +0700

    Test express run

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Yudhistira M' });
+  res.render('index', { title: 'Yudhistira' });
 });
 
 module.exports = router;
